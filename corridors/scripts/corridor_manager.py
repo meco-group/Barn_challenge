c5621cd12c61856e33623ea6033a181f0f65b5f7
Louis Callens <u0143705@kuleuven.be>
2023-04-14 16:46:04 +0200

    updating transformation of corridors with Alejandros work

--- corridors/scripts/corridor_manager.py
+++ corridors/scripts/corridor_manager.py
@@ -23,28 +23,49 @@ class odomMsgClass():
         self.posy = 0.0
         self.theta = 0.0
 
+### OLD ###
+# def transformCorridorToWorld(newCorridor, curr_pose):
+#     # translate the center
+#     center = [newCorridor.center[0] + curr_pose.posx, newCorridor.center[1] + curr_pose.posy]
+
+#     # rotate the tilt
+#     rotation_angle = curr_pose.theta - np.pi/2
+#     tilt = newCorridor.tilt + rotation_angle
+
+#     # translate and rotate the growth center
+#     a = newCorridor.growth_center[0] + curr_pose.posx
+#     b = newCorridor.growth_center[1] + curr_pose.posy
+#     c = a*np.cos(rotation_angle)-b*np.sin(rotation_angle)
+#     d = b*np.cos(rotation_angle)+a*np.sin(rotation_angle)
+#     growth_center = [c, d]
+
+#     transformed_corridor = Corridor(center=center, height=newCorridor.height, width=newCorridor.width)
+#     transformed_corridor.growth_center = growth_center
+
+#     # update W and the corners
+#     transformed_corridor.update_W()
+#     transformed_corridor.corners = get_corners(transformed_corridor.W)
+
+#     return transformed_corridor
+
 def transformCorridorToWorld(newCorridor, curr_pose):
-    # translate the center
-    center = [newCorridor.center[0] + curr_pose.posx, newCorridor.center[1] + curr_pose.posy]
+    # Correct transformation by Alejandro
+    L = np.sqrt(newCorridor.center[0]**2 + newCorridor.center[1]**2)
+    phi = np.arctan2(newCorridor.center[1],newCorridor.center[0]) - np.pi/2 + curr_pose.theta
+    center = [curr_pose.posx - L*np.sin(phi), curr_pose.posy + L*np.cos(phi)]
+    
+    tilt = newCorridor.tilt + curr_pose.theta
 
-    # rotate the tilt
-    rotation_angle = curr_pose.theta - np.pi/2
-    tilt = newCorridor.tilt + rotation_angle
-
-    # translate and rotate the growth center
-    a = newCorridor.growth_center[0] + curr_pose.posx
-    b = newCorridor.growth_center[1] + curr_pose.posy
-    c = a*np.cos(rotation_angle)-b*np.sin(rotation_angle)
-    d = b*np.cos(rotation_angle)+a*np.sin(rotation_angle)
-    growth_center = [c, d]
-
-    transformed_corridor = Corridor(center=center, height=newCorridor.height, width=newCorridor.width)
+    L_growth = np.sqrt(newCorridor.growth_center[0]**2 + newCorridor.growth_center[1]**2)
+    phi_growth = np.arctan2(newCorridor.growth_center[1],newCorridor.growth_center[0]) - np.pi/2 + curr_pose.theta
+    growth_center = [curr_pose.posx - L_growth*np.sin(phi_growth), curr_pose.posy + L_growth*np.cos(phi_growth)] 
+    
+    transformed_corridor = Corridor(center=center, tilt=tilt, heigth=newCorridor.heigth, width=newCorridor.width)
     transformed_corridor.growth_center = growth_center
-
-    # update W and the corners
+    
     transformed_corridor.update_W()
-    transformed_corridor.corners = get_corners(transformed_corridor.W)
-
+    transformed_corridor.corners = newCorridor.get_corners()
+    
     return transformed_corridor
 
 def processNewCorridor(newCorridor, curr_pose, root_corridor, current_corridor):
