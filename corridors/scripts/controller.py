4d3b0c737d73f20f586c42dfa0454e7fdad293d2
Bastiaan Vandewal <bastiaan.vandewal@kuleuven.be>
2023-05-16 14:58:51 +0200

    avoid stopping while rotating at low speeds

--- corridors/scripts/controller.py
+++ corridors/scripts/controller.py
@@ -165,8 +165,12 @@ def main():
         if len(v_full) > 0:
             v_next = v_full.pop(0)
             w_next = w_full.pop(0)
-            twist.linear.x = v_next*smoother + v_previous*(1-smoother)
             twist.angular.z = w_next*smoother + w_previous*(1-smoother)
+            if (abs(twist.angular.z) > 0.2) and (abs(v_next) < 0.1):
+                twist.linear.x = 0
+            else:
+                twist.linear.x = v_next*smoother + v_previous*(1-smoother)
+
             v_previous = twist.linear.x
             w_previous = twist.angular.z
         else:
